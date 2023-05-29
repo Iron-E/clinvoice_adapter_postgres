4cabf915e009d7a14eea41bec6b4054d5e122197
Iron-E <code.iron.e@gmail.com>
2023-05-29 16:59:15 -0400

    fix(Adapter): associated type `Db` doesn't exist

--- src/schema/adapter.rs
+++ src/schema/adapter.rs
@@ -1,14 +1,24 @@
 use sqlx::Postgres;
 use winvoice_adapter::schema::Adapter;
-use super::{PgSchema, PgContact, PgEmployee, PgTimesheet, PgOrganization, PgLocation, PgJob, PgExpenses};
 
-impl Adapter for PgSchema {
-    type Db = Postgres;
-    type Contact = PgContact;
-    type Employee = PgEmployee;
-    type Expenses = PgExpenses;
-    type Job = PgJob;
-    type Location = PgLocation;
-    type Organization = PgOrganization;
-    type Timesheet = PgTimesheet;
+use super::{
+	PgContact,
+	PgEmployee,
+	PgExpenses,
+	PgJob,
+	PgLocation,
+	PgOrganization,
+	PgSchema,
+	PgTimesheet,
+};
+
+impl Adapter for PgSchema
+{
+	type Contact = PgContact;
+	type Employee = PgEmployee;
+	type Expenses = PgExpenses;
+	type Job = PgJob;
+	type Location = PgLocation;
+	type Organization = PgOrganization;
+	type Timesheet = PgTimesheet;
 }
