434f0a5f78c5ed10e5bc68d45bec1b93b3ea2929
Iron-E <code.iron.e@gmail.com>
2023-10-17 18:05:19 -0400

    build!: update `winvoice-adapter`, `winvoice-match`

--- src/fmt/timestamptz/from.rs
+++ src/fmt/timestamptz/from.rs
@@ -1,4 +1,16 @@
+use winvoice_schema::chrono::{DateTime, TimeZone};
+
 use super::{NaiveDateTime, PgTimestampTz};
+
+impl<Tz> From<DateTime<Tz>> for PgTimestampTz
+where
+	Tz: TimeZone,
+{
+	fn from(date: DateTime<Tz>) -> Self
+	{
+		Self::from(date.naive_local())
+	}
+}
 
 impl From<NaiveDateTime> for PgTimestampTz
 {
--- src/schema/initializable.rs
+++ src/schema/initializable.rs
@@ -190,7 +190,7 @@ where
 	sqlx::query!(
 		"CREATE TABLE IF NOT EXISTS job_departments
 		(
-			department_id uuid REFERENCES departments(id),
+			department_id uuid REFERENCES departments(id) ON DELETE CASCADE,
 			job_id uuid REFERENCES jobs(id) ON DELETE CASCADE,
 			PRIMARY KEY (department_id, job_id)
 		);"
